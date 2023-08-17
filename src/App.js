18ab9dbc5c29d1f5c68a8035e7430bc6e03ff1c6
sara1989mohammadi <sara.mohammadi0612@gmail.com>
2023-08-17 22:04:08 +0200

    add api , use effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -49,10 +49,17 @@ const tempWatchedData = [
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-
+const KEY = "7a607a88";
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search));
+  }, []);
+  
   return (
     <>
       <NavBar>
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    {/* <StarRating maxRating={5} /> */}
+    <App />
   </React.StrictMode>
 );
