01a0e276df687b36deea2b2188f71b4872fc97e6
sara1989mohammadi <sara.mohammadi0612@gmail.com>
2023-08-24 19:46:47 +0200

    Cleaning Up Data Fetching

--- src/App.js
+++ src/App.js
@@ -28,11 +28,13 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
           if (!res.ok)
             throw new Error("Something went wrong with fetching movies");
@@ -42,7 +44,9 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found!");
           setMovies(data.Search);
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -53,6 +57,9 @@ export default function App() {
         return;
       }
       fetchMovies();
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
