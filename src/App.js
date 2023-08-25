dd605cea4628713aeff4fd669b5d423692e5f138
sara1989mohammadi <sara.mohammadi0612@gmail.com>
2023-08-25 16:19:56 +0200

    add Close Movie detailes after search

--- src/App.js
+++ src/App.js
@@ -56,6 +56,8 @@ export default function App() {
         setError("");
         return;
       }
+      handelCloseMovie();
+
       fetchMovies();
       return function () {
         controller.abort();
